425fc0f563ffd75ac5d622793111ac5f374210bd
hp77 <u19ee064@eed.svnit.ac.in>
2021-02-10 23:25:12 +0530

    Add SemVer facility to check if reference is valid

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import fetch from 'node-fetch';
+import semver from 'semver';
 
-async function fetchPackage(reference){
+async function fetchPackage({name, reference}){
+    if(semver.valid(reference)){
+        return await fetchPackage(
+            {
+                name,
+                reference: `https://registry.yarnpkg.com/${name}/-/${name}-${reference}.tgz`,
+            }
+        );
+    }
     let response = await fetch(reference);
 
     if(!response.ok) throw new Error(`Could not fetch the package "${reference}"`);
