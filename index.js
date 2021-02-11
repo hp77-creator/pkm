96392b4899337eb3ba931072199b7c2812925aa3
hp77 <u19ee064@eed.svnit.ac.in>
2021-02-11 18:41:51 +0530

    Update with addition of pinned references

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ import fetch from 'node-fetch';
 import semver from 'semver';
 import fs from 'fs-extra';
 
+
+async function getPinnedReferences({name, reference}){
+    if(semver.validRange(reference) && !semver.valid(reference)){
+        let response = await fetch(`https://registry.yarnpkg.com/${name}`);
+        let info = await response.json();
+
+        let versions = Object.keys(info.versions);
+        let maxSatisfying = semver.maxSatisfying(versions, reference);
+
+        if(maxSatisfying == null) throw new Error(
+            `Couldn't find any package with the name "${name}" matching reference "${reference}"`
+        );
+
+        reference = maxSatisfying;
+    }
+
+    return {name, reference};
+
+    
+}
+
 async function fetchPackage({name, reference}){
     if(semver.valid(reference)){
         return await fetchPackage(
